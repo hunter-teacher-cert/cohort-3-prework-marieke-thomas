99a2bf33116a2288bf86c2b8d1f44d1a673ca484
mthomas42 <>
2022-06-28 19:19:26 +0000

    Finished pre07

--- pre07/Sieve.java
+++ pre07/Sieve.java
@@ -3,19 +3,23 @@ import java.util.*;
 
 public class Sieve{
   public static void main(String[] args){
-    System.out.println("Hello world");
-    sieve(20);
+    System.out.println(Arrays.toString(sieve(20)));
   }
   public static boolean[] sieve(int n){
-    int[] isPrime = new int[n];
-    for (i=0; i<n; i++){
+    boolean[] isPrime = new boolean[n];
+    for (int i=0; i<n; i++){
       isPrime[i] = true;
     }
     isPrime[0] = false;
     isPrime[1] = false;
     int limit = (int)Math.sqrt(n) + 1;
-    for (i=2; i<limit; i++){
-      System.out.println(i);
+    for (int i=2; i<limit; i++){
+      if (isPrime[i]){
+        for (int j = i*i; j<n; j += i){
+          isPrime[j] = false;
+        }
+      }
     }
+    return isPrime;
   }
 }
